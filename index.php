eb1b652cff28dcbc97f8c7c8ed87805eb6771888
maldonado2003 <40504578+maldonado2003@users.noreply.github.com>
2020-08-20 10:26:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
 echo "My first PHP script!";
 ?>
 
+<h1>Hello World</h1>
+<p><a href="https://2f406d15e3af.ngrok.io/public/read.php?partno=%s">Visit Inventory!</a></p>
+<p>I'm hosted with GitHub Pages.</p>
+<p>I'm hosted with GitHub Pages.</p>
+<p>I'm hosted with GitHub Pages.</p>
+
 </body>
 </html>
